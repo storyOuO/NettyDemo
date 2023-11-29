d4fad32176d3e98253a9f5fb871418c2bbc4fad2
storyOuO <1216938059@qq.com>
2023-11-29 21:37:31 +0800

    feat:NIO模型测试

--- src/main/java/nio/BasicBuffer.java
+++ src/main/java/nio/BasicBuffer.java
@@ -1,4 +1,26 @@
 package nio;
 
+import java.nio.IntBuffer;
+
 public class BasicBuffer {
+
+    public static void main(String[] args) {
+
+        // 初始化IntBuffer 当前容量为5
+        IntBuffer intBuffer = IntBuffer.allocate(5);
+
+        // 放入缓存
+        for (int i = 0; i < intBuffer.capacity(); i++) {
+            intBuffer.put(i);
+        }
+
+        // 读写切换 否则取不出数据!!
+        intBuffer.flip();
+
+        // 取出缓存
+        while (intBuffer.hasRemaining()) {
+            System.out.println("取出缓存: "+intBuffer.get());
+        }
+
+    }
 }
